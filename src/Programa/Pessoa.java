4c6faa8353e0a0a76c9e3ef4018e7d2f318fcde9
Arthur Silva <arthurcom898@gmail.com>
2025-09-15 08:49:37 -0300

    Generating our getters and setters constructor

--- src/Programa/Pessoa.java
+++ src/Programa/Pessoa.java
@@ -7,4 +7,23 @@ public class Pessoa {
     private String nome;
     private String CPF;
     private String email;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCPF() {
+        return CPF;
+    }
+    public void setCPF(String cPF) {
+        CPF = cPF;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
